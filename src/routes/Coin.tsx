b14535a3f9f2b270bf6f61a2e72e64fb23084f1b
Ghunhee Shin <hago@rocketstaff.com>
2022-04-22 12:00:19 +0900

    Feat(Route State) : "추가 Route Link State 전송"
    router의 link를 통해 state를 전송할 수 있다. state로 전송된 데이터는 컴포넌트가 생성되기전에 데이터가 들어가기 때문에 빠른 표시가 가능하다.
    state를 통해 넘긴 데이터는 useLocation()을 통해 사용이 가능한데, 해당 타입을 설정해주기 위해 interface를 지정할 때에는
    interface OOO {
         state: {
               지정한 속성 : 값
          }
    }
    의 형태로 인터페이스를 설정한 뒤, 'useLocation() as 인터페이스명'
    으로 해당 인터페이스를 적용시켜줄 수 있다.

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,9 +1,54 @@
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+    padding: 0px 20px;
+    max-width: 480px;
+    margin: 0 auto;
+`;
+
+const Header = styled.header`
+    height: 10vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Title = styled.h1`
+    color: ${(props) => props.theme.accentColor};
+    font-size: 48px;
+`;
+
+const Loader = styled.span`
+    text-align: center;
+    display: block;
+`;
+
+//Link의 state를 사용하여 데이터를 넘겨줄 때 interface는
+//state를 한 번 감싸도록 만들어주고
+//useLocation()으로 불러올 때 'as 인터페이스명'으로
+interface RouteState {
+    state: {
+        name: string;
+    };
+}
 
 function Coin() {
-    const {coinId} = useParams();
-
-    return <h1>Coin: {coinId}</h1>;
+    const { coinId } = useParams();
+    const [loading, setLoading] = useState(true);
+    //단 Link의 state를 통해 들어오는 데이터의 경우,
+    //해당 주소로 바로 들어올 때에는 그 값을 받아오지 못한다.
+    //현재 코드의 경우, 주소로 바로 들어올 경우 title에도 Loading만 뜨게된다.
+    const { state } = useLocation() as RouteState;
+    return (
+        <Container>
+            <Header>
+                <Title>{state?.name || "Loading..."}</Title>
+            </Header>
+            {loading ? <Loader>Loading...</Loader> : null}
+        </Container>
+    );
 }
 
 export default Coin;
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -25,13 +25,20 @@ const Coin = styled.li`
     a {
         padding: 20px;
         transition: color 0.2s ease-in;
-        display: block;
+        display: flex;
+        align-items: center;
     }
     &:hover {
         a {
             color: ${(props) => props.theme.accentColor};
         }
     }
+`;
+
+const Image = styled.img`
+    width: 25px;
+    height: 25px;
+    margin-right: 10px;
 `;
 
 const Loader = styled.span`
@@ -81,7 +88,15 @@ function Coins() {
                 <CoinsList>
                     {coins.map((coin) => (
                         <Coin key={coin.id}>
-                            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+                            <Link
+                                to={`/${coin.id}`}
+                                state={{ name: coin.name }}
+                            >
+                                <Image
+                                    src={`https://cryptocurrencyliveprices.com/img/${coin.id}.png`}
+                                />
+                                {coin.name} &rarr;
+                            </Link>
                         </Coin>
                     ))}
                 </CoinsList>
